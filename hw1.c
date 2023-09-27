b05fb439947cf23cab67dc29f6e8ab57daa084c9
hungyuhang <345genius@gmail.com>
2023-09-27 11:48:47 +0800

    Change typesetting of hw1.c

--- hw1.c
+++ hw1.c
@@ -1,35 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint16_t count_leading_zeros(uint64_t x)
-{
-	x |= (x >> 1);
-	x |= (x >> 2);
-	x |= (x >> 4);
-	x |= (x >> 8);
-	x |= (x >> 16);
-	x |= (x >> 32);
-	
-	/* count ones (population count) */
-	x -= ((x >> 1) & 0x5555555555555555 /* Fill this! */ );
-	x = ((x >> 2) & 0x3333333333333333) + (x & 0x3333333333333333 /* Fill this! */);
-	x = ((x >> 4) + x) & 0x0f0f0f0f0f0f0f0f;
-	x += (x >> 8);
-	x += (x >> 16);
-	x += (x >> 32);
-	
-	return (64 - (x & 0x7f));
-}
-
-bool predict_if_mul_overflow(uint64_t *x0, uint64_t *x1)
-{
-	int32_t exp_x0 = 63 - (int32_t)count_leading_zeros(*x0);
-	int32_t exp_x1 = 63 - (int32_t)count_leading_zeros(*x1);
-	if ((exp_x0 + 1) + (exp_x1 + 1) >= 64)
-		return true;
-	else
-		return false;
-}
 
 // test case a: no overflow, predict result is false
 uint64_t a_x0 = 0x0000000000000000;
@@ -45,11 +16,41 @@ uint64_t d_x0 = 0x0000000000000003;
 uint64_t d_x1 = 0x7FFFFFFFFFFFFFFF;
 
 
+uint16_t count_leading_zeros(uint64_t x)
+{
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+    x |= (x >> 32);
+
+    /* count ones (population count) */
+    x -= ((x >> 1) & 0x5555555555555555);
+    x = ((x >> 2) & 0x3333333333333333) + (x & 0x3333333333333333);
+    x = ((x >> 4) + x) & 0x0f0f0f0f0f0f0f0f;
+    x += (x >> 8);
+    x += (x >> 16);
+    x += (x >> 32);
+
+    return (64 - (x & 0x7f));
+}
+
+bool predict_if_mul_overflow(uint64_t *x0, uint64_t *x1)
+{
+    int32_t exp_x0 = 63 - (int32_t)count_leading_zeros(*x0);
+    int32_t exp_x1 = 63 - (int32_t)count_leading_zeros(*x1);
+    if ((exp_x0 + 1) + (exp_x1 + 1) >= 64)
+        return true;
+    else
+        return false;
+}
+
 void main()
 {
-	printf("%d\n", predict_if_mul_overflow(&a_x0, &a_x1));
+    printf("%d\n", predict_if_mul_overflow(&a_x0, &a_x1));
     printf("%d\n", predict_if_mul_overflow(&b_x0, &b_x1));
     printf("%d\n", predict_if_mul_overflow(&c_x0, &c_x1));
     printf("%d\n", predict_if_mul_overflow(&d_x0, &d_x1));
-	return;
+    return;
 }
